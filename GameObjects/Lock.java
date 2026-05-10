48d14eb105a307ca8c96a0fa3f11b24da79fa7e1
gmammadli <memmedliguluzar2007@gmail.com>
2026-05-10 17:55:55 +0400

    Update Lock.java
    Проверяет ключ

--- GameObjects/Lock.java
+++ GameObjects/Lock.java
@@ -9,6 +9,15 @@ public class Lock {
 
     public boolean isLocked() { return isLocked; }
 
+    // it allows to open the lock with a key
+    public boolean unlock(GameObject item) {
+        if (item instanceof Key) {
+            this.isLocked = false;
+            return true;
+        }
+        return false;
+    }
+
     public void setLocked(boolean locked) {
         this.isLocked = locked;
     }
